bab34cad87a9cd017d73397311b798b55f4565f3
shettynitin667 <shettynitin667@gmail.com>
2020-07-06 14:37:50 +0530

    Made changes to card design and solve request timeout issue

--- index.js
+++ index.js
@@ -13,33 +13,37 @@ app.get('/',(req,res)=>{
 
 //Search results page
 app.get('/results', (req,res)=>{
-    var searchElement = 'http://www.omdbapi.com/?s='+req.query.movie+'&apikey=thewdb';
-    let movieDetails = [];
-    let results;
+    var searchElement = 'https://www.omdbapi.com/?s='+req.query.movie+'&apikey=thewdb';
+    var movieDetails = [];
+    var results;
     rp(searchElement)
     .then((body)=>{
-        results = JSON.parse(body);     
-        for(let i=0; i< results['Search'].length; i++){
-            rp('http://www.omdbapi.com/?i='+results['Search'][i]['imdbID']+'&apikey=thewdb')
-            .then(data => {
-                movieDetails.push(JSON.parse(data));
-                if(movieDetails.length === results['Search'].length){
-                    res.render('movieResults',{results: results, keyword: req.query.movie, movieDetails: movieDetails});
-                }
-            }) 
-        }   
+        results = JSON.parse(body);  
+        if(results['Response']=='True'){
+            for(let i=0; i< results['Search'].length; i++){
+                rp('https://www.omdbapi.com/?i='+results['Search'][i]['imdbID']+'&apikey=thewdb')
+                .then(data => {
+                    movieDetails.push(JSON.parse(data));
+                    if(movieDetails.length === results['Search'].length){
+                        res.render('movieResults',{results: results, keyword: req.query.movie, movieDetails: movieDetails});
+                    }
+                }) 
+            }
+        }
+        else{
+            res.render('movieResults',{results: results, keyword: req.query.movie, movieDetails: movieDetails});
+        }        
     })
     .catch((err)=>{
-        console.log(err.message);
-        res.render('movieResults',{results: {'Response': 'False'}, keyword: req.query.movie});
+        var displayError = JSON.parse(err['error']);
+        console.log(displayError['Error']);
+        res.render('movieResults',{results: {'Response': 'False', 'Error': displayError['Error']}, keyword: req.query.movie});
     });
 
 });
 
-app.get('/results/:id', (req,res)=>{
-
-});
 
 app.listen(3000,()=>{
     console.log('Listening on Port 3000')
 });
+
